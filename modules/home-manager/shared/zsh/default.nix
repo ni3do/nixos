ca533f3d39e610e5a43e8e18a7d68e5575b2dce7
niedo <57731234+ni3do@users.noreply.github.com>
2025-02-04 16:16:51 +0100

    auto-commit

--- modules/home-manager/shared/zsh/default.nix
+++ modules/home-manager/shared/zsh/default.nix
@@ -1,37 +1,38 @@
-{
-  config,
-  vars,
-  ...
-}:
-
-let
-  systemSpecificRebuildCmd =
-    if (vars.system == "aarch64-darwin") then "darwin-rebuild" else "sudo nixos-rebuild";
-in
+{ ... }:
 {
   programs.zsh = {
     enable = true;
-    envExtra = ''
-      ZDOTDIR="${config.home.homeDirectory}/.config/zsh"
-      alias rnix="${systemSpecificRebuildCmd} switch --flake /etc/nixos --impure"
-    '';
-    ohMyZsh = {
+    initExtra = "eval \"$(micromamba shell hook --shell zsh)\"";
+    oh-my-zsh = {
       enable = true;
       theme = "powerlevel10k/powerlevel10k";
       plugins = [
         "zsh-autosuggestions"
         "zsh-completions"
         "zsh-syntax-highlighting"
-        "zsh-starship"
       ];
+    };
+    shellAliases = {
+      # aliases for common dirs
+      home = "cd ~";
+      # misc  aliases
+      x = "exit";
+      vi = "nvim";
+      mm = "micromamba";
+      # git aliases
+      gss = "git status --short";
+      stat = "git status";
+      gaa = "git add .";
+      gac = "git add .; git commit -m 'auto-commit'; git push";
+      gacpp = "git add .; git commit -m 'auto-commit'; git push; ssh disco-world 'cd /itet-stor/siwachte/net_scratch/fingnn && git pull'";
+      # keybinding help
+      # "helpskhd" = "cat ~/.config/skhd/skhdrc";
+      # "helpyabai" = "cat ~/.config/yabai/yabairc";
     };
   };
 
   home.file.".config/zsh" = {
     source = ./config;
     recursive = true;
-  };
-  home.file.".zshrc" = {
-    source = ./config/.zshrc;
   };
 }
