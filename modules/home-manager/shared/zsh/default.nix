cb5dcf4452888484e4032d25277ca6fc80e68474
niedo <57731234+ni3do@users.noreply.github.com>
2025-02-04 16:28:58 +0100

    auto-commit

--- modules/home-manager/shared/zsh/default.nix
+++ modules/home-manager/shared/zsh/default.nix
@@ -2,37 +2,18 @@
 {
   programs.zsh = {
     enable = true;
+    enableCompletion = true;
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
     initExtra = "eval \"$(micromamba shell hook --shell zsh)\"";
-    oh-my-zsh = {
-      enable = true;
-      theme = "powerlevel10k/powerlevel10k";
-      plugins = [
-        "zsh-autosuggestions"
-        "zsh-completions"
-        "zsh-syntax-highlighting"
-      ];
-    };
-    shellAliases = {
-      # aliases for common dirs
-      home = "cd ~";
-      # misc  aliases
-      x = "exit";
-      vi = "nvim";
-      mm = "micromamba";
-      # git aliases
-      gss = "git status --short";
-      stat = "git status";
-      gaa = "git add .";
-      gac = "git add .; git commit -m 'auto-commit'; git push";
-      gacpp = "git add .; git commit -m 'auto-commit'; git push; ssh disco-world 'cd /itet-stor/siwachte/net_scratch/fingnn && git pull'";
-      # keybinding help
-      # "helpskhd" = "cat ~/.config/skhd/skhdrc";
-      # "helpyabai" = "cat ~/.config/yabai/yabairc";
-    };
-  };
-
-  home.file.".config/zsh" = {
-    source = ./config;
-    recursive = true;
+    # oh-my-zsh = {
+    #   enable = true;
+    #   theme = "powerlevel10k/powerlevel10k";
+    #   plugins = [
+    #     "zsh-autosuggestions"
+    #     "zsh-completions"
+    #     "zsh-syntax-highlighting"
+    #   ];
+    # };
   };
 }
