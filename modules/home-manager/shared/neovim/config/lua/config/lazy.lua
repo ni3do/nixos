696663f013763044b96d5bb965fbd061fa5ae7d3
niedo <57731234+ni3do@users.noreply.github.com>
2025-02-10 15:42:11 +0100

    auto-commit

--- modules/home-manager/shared/neovim/config/lua/config/lazy.lua
+++ modules/home-manager/shared/neovim/config/lua/config/lazy.lua
@@ -25,7 +25,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.lang.git" },
     { import = "lazyvim.plugins.extras.lang.json" },
-    { import = "lazyvim.plugins.extras.lang.markdown" },
+    -- { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.lang.nix" },
     { import = "lazyvim.plugins.extras.lang.python" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
