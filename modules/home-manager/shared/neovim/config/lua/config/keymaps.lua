ce9e59117812e931d24771cbb912d57096ab7183
niedo <57731234+ni3do@users.noreply.github.com>
2025-02-13 11:33:26 +0100

    auto-commit

--- modules/home-manager/shared/neovim/config/lua/config/keymaps.lua
+++ modules/home-manager/shared/neovim/config/lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("n", "<leader>fr", function()
+  Snacks.terminal(nil, { win = { position = "right" } })
+end, { desc = "Terminal right" })
+
+vim.keymap.set("n", "<leader>o", ":ObsidianQuickSwitch<CR>", { desc = "Open Obsidian Quick Switch" })
