d3461534d8cb201f4496299edb67bd084a4e9844
niedo <57731234+ni3do@users.noreply.github.com>
2025-04-26 09:56:55 +0200

    auto-commit

--- modules/darwin/brew.nix
+++ modules/darwin/brew.nix
@@ -23,6 +23,7 @@
     ];
     casks = [
       { name = "mocki-toki/formulae/barik"; greedy = true; }
+      { name = "chatgpt"; greedy = true; }
       { name = "cursor"; greedy = true; }
       { name = "firefox"; greedy = true; }
       { name = "font-sf-pro"; greedy = true; }
@@ -48,6 +49,7 @@
       "AdGuard for Safari" = 1440147259;
       "Bitwarden" = 1352778147; 
       "Keynote" = 409183694;
+      "Reeder" = 6475002485;
       "Vimari" = 1480933944;
       "Wireguard" = 1451685025;
       };
