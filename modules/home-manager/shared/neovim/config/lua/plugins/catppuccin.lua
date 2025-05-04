032f8dc84ec8f676035c1f694ea12fb9dafce460
niedo <57731234+ni3do@users.noreply.github.com>
2025-05-04 16:48:27 +0200

    auto-commit

---
+++ modules/home-manager/shared/neovim/config/lua/plugins/catppuccin.lua
@@ -0,0 +1,5 @@
+return { "catppuccin/nvim", name = "catppuccin", priority = 1000,    
+	config = function()
+	      -- load the colorscheme here
+	      vim.cmd([[colorscheme catppuccin-macchiato]])
+    end, }
