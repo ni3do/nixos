1b5527db3a76b51138ed6059840ca91e3c7f34b0
niedo <57731234+ni3do@users.noreply.github.com>
2025-05-07 14:00:09 +0200

    auto-commit

--- modules/home-manager/shared/neovim/config/lua/config/options.lua
+++
@@ -1,9 +0,0 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
-
-vim.opt.colorcolumn = "120"
-vim.opt.textwidth = 120
-vim.opt.scrolloff = 10
-vim.g.vimtex_view_method = "zathura"
-vim.g.vimtex_view_zathura_use_synctex = 0
