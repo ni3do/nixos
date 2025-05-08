7e15806dc77d766c683def110a10c891741c3040
niedo <57731234+ni3do@users.noreply.github.com>
2025-05-08 18:48:58 +0200

    auto-commit

--- modules/darwin/brew.nix
+++ modules/darwin/brew.nix
@@ -4,42 +4,58 @@
   homebrew = {
     enable = true;
     onActivation = {
-      autoUpdate = false;
+      autoUpdate = true;
       upgrade = true;
       cleanup = "zap";
     };
 
     taps = [
       "felixkratz/formulae"
+      "koekeishiya/formulae"
     ];
     brews = [
-      "coreutils"
+      "borders"
       "kanata"
+      # sketchybar dependency
       "lua"
       "mas"
       "rust"
+      # sketchybar dependency
+      "nowplaying-cli"
       "sketchybar"
+      "skhd"
+      # terminal prompt
+      "starship"
+      # sketchybar dependency
+      "switchaudio-osx"
+      "yabai"
     ];
     casks = [
+      # browser
+      { name = "firefox"; greedy = true; }
+      # terminal
+      { name = "ghostty"; greedy = true; }
+      # productivity
       { name = "chatgpt"; greedy = true; }
       { name = "cursor"; greedy = true; }
-      { name = "firefox"; greedy = true; }
+      { name = "obsidian"; greedy = true; }
+      { name = "raycast"; greedy = true; }
+      { name = "unnaturalscrollwheels"; greedy = true; }
+      { name = "visual-studio-code"; greedy = true; }
+      { name = "karabiner-elements"; greedy = true; }
+      # util
       { name = "macs-fan-control"; greedy = true; }
       { name = "nordvpn"; greedy = true; } 
-      { name = "obsidian"; greedy = true; }
       { name = "spotify"; greedy = true; }
       { name = "telegram-desktop"; greedy = true; }
-      { name = "visual-studio-code"; greedy = true; }
       { name = "whatsapp"; greedy = true; }
-      { name = "karabiner-elements"; greedy = true; }
       # fonts
-      { name = "font-jetbrains-mono-nerd-font"; greedy = true; }
       { name = "font-sf-pro"; greedy = true; }
+      { name = "font-jetbrains-mono-nerd-font"; greedy = true; }
     ];
     masApps = {
       "AdGuard for Safari" = 1440147259;
       "Bitwarden" = 1352778147; 
-      "Keynote" = 409183694;
       "Vimari" = 1480933944;
       "Wireguard" = 1451685025;
       };
--- modules/darwin/pkgs.nix
+++ modules/darwin/pkgs.nix
@@ -1,27 +1,16 @@
 { pkgs, ... }:
 
 {
-  programs.zsh.enable = true;
 
   nixpkgs.config.allowUnfree = true;
 
-  # TODO: get fronts from packages rather than from brew
-  # fonts.packages = with pkgs; [
-  #   nerd-fonts.jetbrains-mono
-  #   nerd-fonts.fira-mono
-  #   nerd-fonts.profont
-  #
-  # ];
-
   environment.systemPackages = with pkgs; [
-    # shell history
+    # better command history
     atuin
     # better cat
     bat
     # better top
     btop
-    # terminal graphics
-    chafa
     discord
     # better ls
     eza
@@ -31,48 +20,25 @@
     fzf
     gh
     git
-    # wallpaper converter / daily wallpaper provider
-    gowall
-    # colored borders for user windows on macos
-    jankyborders
-    # terminal emulator
-    kitty
     # pokemon image cli
     krabby
-    lua
     # python env and package manager
     micromamba
     neovim
-    nodejs_23
-    # sketchybar dependency
-    nowplaying-cli
-    oh-my-zsh
+    nodejs_22
     pre-commit
     python3
     # fzf dependency
     ripgrep
-    # signal-desktop
-    # keyboard shortcut daemon for macos
-    skhd
-    spotify
-    # terminal prompt
-    starship
-    # sketchybar dependency
-    switchaudio-osx
-    telegram-desktop
     texliveFull
     # Latex formatter
     tex-fmt
     tmux
-    # invert scroll direction of physical scroll wheels
-    unnaturalscrollwheels
-    vscode
     wget
-    # whatsapp-for-mac
-    # tiling window manager for macos
-    yabai
     # pdf viewer
     zathura
+    # smart cd
+    zoxide
     zsh
   ];
 }
